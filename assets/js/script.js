69e595599229917139bed638c929b36f4dd4ad8b
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-10-28 09:54:21 -0400

    local storage items display on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,37 @@ var todaysDate = moment().format("dddd MMM Do h:m A");
 var tasks = {};
 var saveBtn =$(".saveBtn");
 
+//display todays date in header
 $("p#currentDay").text(todaysDate);
 
+//capture plan task and time with save button click for that time block
 saveBtn.on("click", function() {
 
     var time = $(this).siblings(".hour").text();
     var plan = $(this).siblings(".planTask").val();
 
-    // THEN the text for that event is saved in local storage
+    // the text/value for save button block is saved in local storage
     localStorage.setItem(time, plan);
-    // console.log(time,plan);
+    
+});
+
+
+//Load tasks from local storage
+
+$(document).ready(function() {
+
+    $(".hour").each(function() {
+        var savedHourBlock = $(this).text();
+        var savedPlanTask = localStorage.getItem(savedHourBlock);
+
+        // console.log(savedHourBlock);
+        // console.log(savedPlanTask);
+
+        if(savedPlanTask !== null) {
+            $(this).siblings(".planTask").val(savedPlanTask);
+            
+        }
+    });
 });
 
 
