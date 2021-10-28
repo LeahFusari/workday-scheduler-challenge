e791bab107cabc840345092bb03791d01f28552e
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-10-28 08:09:03 -0400

    Got click event on save buttons to work and capture timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,55 @@
-var todaysDate = moment().format("dddd MMM Do");
+var todaysDate = moment().format("dddd MMM Do h:m A");
+var tasks = {};
+var saveBtn =$(".saveBtn");
 
 $("p#currentDay").text(todaysDate);
+
+saveBtn.on("click", function() {
+
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".planTask").val();
+
+    // THEN the text for that event is saved in local storage
+    localStorage.setItem(time, plan);
+    console.log(time,plan)
+});
+
+
+
+
+
+// $("#textarea-9A").click(function() {
+    
+//     });
+
+// $("#textarea-10A").click(function() {
+    
+//     });
+
+// $("#textarea-11A").click(function() {
+    
+//     });
+
+// $("#textarea-12P").click(function() {
+   
+//    });
+   
+//    $("#textarea-1P").click(function() {
+
+//    });
+   
+//    $("#textarea-2P").click(function() {
+
+//    }); 
+
+//    $("#textarea-3P").click(function() {
+
+//    });
+   
+//    $("#textarea-4P").click(function() {
+
+//    });
+   
+//    $("#textarea-5P").click(function() {
+       
+//    });    
